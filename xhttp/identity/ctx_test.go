1d368d599e5d91523b6d03c358657055ef308661
Denis Issoupov <denis@ekspand.com>
2018-10-01 21:13:27 -0700

    Added WithTestIdentity

--- xhttp/identity/ctx_test.go
+++ xhttp/identity/ctx_test.go
@@ -1,6 +1,9 @@
 package identity
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -8,6 +11,7 @@ import (
 
 	"github.com/go-phorce/dolly/xhttp/header"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMain(m *testing.M) {
@@ -47,4 +51,37 @@ func Test_RoleContext(t *testing.T) {
 	assert.Equal(t, "bob_1/"+nodeInfo.HostName(), c.Identity().String())
 	assert.Equal(t, nodeInfo.HostName(), c.Identity().Name())
 	assert.Equal(t, "bob_1", c.Identity().Role())
+}
+
+func Test_TestIdentity(t *testing.T) {
+	h := func(w http.ResponseWriter, r *http.Request) {
+		ctx := ForRequest(r)
+		identity := ctx.Identity()
+		responseBody := fmt.Sprintf("{\"role\": \"%s\", \"name\": \"%s\" }", identity.Role(), identity.Name())
+		io.WriteString(w, responseBody)
+	}
+
+	server := httptest.NewServer(http.HandlerFunc(h))
+	defer server.Close()
+
+	r, err := http.NewRequest(http.MethodGet, "/", nil)
+	require.NoError(t, err)
+	identity := NewIdentity("enrollme_dev", "localhost")
+	r = WithTestIdentity(r, identity)
+
+	w := httptest.NewRecorder()
+	h(w, r)
+	require.Equal(t, http.StatusOK, w.Code)
+
+	type rt struct {
+		Role string `json:"role,omitempty"`
+		Name string `json:"name,omitempty"`
+	}
+
+	res := &rt{}
+	body := w.Body.Bytes()
+	s := string(body)
+	assert.NoError(t, json.Unmarshal(body, res))
+	assert.Equal(t, identity.Role(), res.Role, s)
+	assert.Equal(t, identity.Name(), res.Name, s)
 }
