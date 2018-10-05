a1347add90878e79b4442a7da7fdef1a0b21e671
Denis Issoupov <dissoupov@salesforce.com>
2018-10-05 11:24:55 -0700

    httperror codes

--- xhttp/httperror/codes.go
+++ xhttp/httperror/codes.go
@@ -1,36 +1,40 @@
 package httperror
 
 const (
-	// InvalidParam is returned where a URL parameter, or other type of generalized parameters value is invalid.
-	InvalidParam = "invalid_parameter"
-
-	// InvalidJSON is returned when we were unable to parse a client supplied JSON Payload.
-	InvalidJSON = "invalid_json"
-
-	// InvalidRequest is returned when the request validation failed.
-	InvalidRequest = "invalid_request"
-
-	// NotFound is returned when the requested URL doesn't exist.
-	NotFound = "not_found"
-
-	// RequestTooLarge is returned when the client provided payload is larger than allowed for the particular resource.
-	RequestTooLarge = "request_too_large"
-
+	// AccountNotFound when requested account not found
+	AccountNotFound = "account_not_found"
+	// BadNonce is returned for bad nonce.
+	BadNonce = "bad_nonce"
+	// Connection is returned when connection failed.
+	Connection = "connection"
+	// ContentLengthRequired is returned when request does not specify ContentLength.
+	ContentLengthRequired = "content_length_required"
 	// FailedToReadRequestBody is returned when there's an error reading the HTTP body of the request.
 	FailedToReadRequestBody = "request_body"
-
-	// RateLimitExceeded is returned when the client has exceeded their request allotment.
-	RateLimitExceeded = "rate_limit_exceeded"
-
-	// Unexpected is returned when something went wrong.
-	Unexpected = "unexpected"
-
 	// Forbidden is returned when the client is not authorized to access the resource indicated.
 	Forbidden = "forbidden"
-
+	// InvalidContentType is returned when request specifies invalid Content-Type.
+	InvalidContentType = "invalid_content_type"
+	// InvalidJSON is returned when we were unable to parse a client supplied JSON Payload.
+	InvalidJSON = "invalid_json"
+	// InvalidParam is returned where a URL parameter, or other type of generalized parameters value is invalid.
+	InvalidParam = "invalid_parameter"
+	// InvalidRequest is returned when the request validation failed.
+	InvalidRequest = "invalid_request"
+	// Malformed is returned when the request was malformed.
+	Malformed = "malformed"
+	// NotFound is returned when the requested URL doesn't exist.
+	NotFound = "not_found"
 	// NotReady is returned when the service is not ready to serve
 	NotReady = "not_ready"
-
+	// RateLimitExceeded is returned when the client has exceeded their request allotment.
+	RateLimitExceeded = "rate_limit_exceeded"
 	// RequestFailed is returned when an outbound request failed.
 	RequestFailed = "request_failed"
+	// RequestTooLarge is returned when the client provided payload is larger than allowed for the particular resource.
+	RequestTooLarge = "request_too_large"
+	// Unauthorized is for unauthorized access.
+	Unauthorized = "unauthorized"
+	// Unexpected is returned when something went wrong.
+	Unexpected = "unexpected"
 )
