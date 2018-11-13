200e993baad851e0b44e6599ebe90a58ff809e54
Denis Issoupov <dissoupov@salesforce.com>
2018-11-13 07:39:52 -0800

    service.Factory interface

--- rest/service.go
+++ rest/service.go
@@ -8,3 +8,6 @@ type Service interface {
 	// IsReady indicates that service is ready to serve its end-points
 	IsReady() bool
 }
+
+// Factory is interface to create Services
+type Factory func(Server) Service
