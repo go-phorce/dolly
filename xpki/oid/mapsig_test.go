463838496b8c7769a9ac0146e9b11d42c25a88e7
Denis Issoupov <denis@ekspand.com>
2018-11-12 21:15:00 -0800

    Coverage: OIDs

--- xpki/oid/mapsig_test.go
+++ xpki/oid/mapsig_test.go
@@ -1,9 +1,18 @@
 package oid
 
 import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/asn1"
 	"testing"
 
+	"github.com/juju/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_SignatureAlgorithmByOID(t *testing.T) {
@@ -23,4 +32,85 @@ func Test_SignatureAlgorithmByOID(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_SignatureAlgorithmInfo(t *testing.T) {
+	s := RSAWithSHA1
+	assert.Equal(t, crypto.SHA1, s.HashFunc())
+	assert.Equal(t, asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 1, 5}, s.OID())
+	assert.Equal(t, "{iso(1) member-body(2) us(840) rsadsi(113549) pkcs(1) pkcs-1(1) sha1-with-rsa-signature(5)}", s.Registration())
+}
+
+func Test_SignatureAlgorithmByName(t *testing.T) {
+	s, err := SignatureAlgorithmByName(RSAWithSHA1.Name())
+	require.NoError(t, err)
+	assert.Equal(t, s, RSAWithSHA1)
+
+	s, err = SignatureAlgorithmByName("invalid")
+	assert.Error(t, err)
+	assert.True(t, errors.IsNotFound(err))
+}
+
+func Test_SignatureAlgorithmByKey(t *testing.T) {
+	_, err := SignatureAlgorithmByKey(0)
+	assert.Error(t, err)
+
+	t.Run("RSA", func(t *testing.T) {
+		key, err := rsa.GenerateKey(rand.Reader, 512)
+		require.NoError(t, err)
+
+		pa, err := SignatureAlgorithmByKey(key)
+		require.NoError(t, err)
+		assert.Equal(t, &RSA, pa)
+
+		sa, err := SignatureAlgorithmByKeyAndHash(key, SHA1.hash)
+		require.NoError(t, err)
+		assert.Equal(t, &RSAWithSHA1, sa)
+
+	})
+	t.Run("ECDSA", func(t *testing.T) {
+		key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+		require.NoError(t, err)
+
+		pa, err := SignatureAlgorithmByKey(key)
+		require.NoError(t, err)
+		assert.Equal(t, &ECDSA, pa)
+
+		_, err = SignatureAlgorithmByKey(0)
+		assert.Error(t, err)
+
+		_, err = SignatureAlgorithmByKeyAndHash(0, SHA1.hash)
+		assert.Error(t, err)
+
+		sa, err := SignatureAlgorithmByKeyAndHash(key, SHA1.hash)
+		require.NoError(t, err)
+		assert.Equal(t, &ECDSAWithSHA1, sa)
+
+		sa, err = SignatureAlgorithmByKeyAndHash(key, SHA256.hash)
+		require.NoError(t, err)
+		assert.Equal(t, &ECDSAWithSHA256, sa)
+
+		sa, err = SignatureAlgorithmByKeyAndHash(key, SHA384.hash)
+		require.NoError(t, err)
+		assert.Equal(t, &ECDSAWithSHA384, sa)
+
+		sa, err = SignatureAlgorithmByKeyAndHash(key, SHA512.hash)
+		require.NoError(t, err)
+		assert.Equal(t, &ECDSAWithSHA512, sa)
+	})
+}
+
+func Test_SignatureAlgorithmByX509(t *testing.T) {
+	assert.Equal(t, &RSAWithSHA1, SignatureAlgorithmByX509(x509.SHA1WithRSA))
+	assert.Equal(t, &RSAWithSHA256, SignatureAlgorithmByX509(x509.SHA256WithRSA))
+	assert.Equal(t, &RSAWithSHA384, SignatureAlgorithmByX509(x509.SHA384WithRSA))
+	assert.Equal(t, &RSAWithSHA512, SignatureAlgorithmByX509(x509.SHA512WithRSA))
+	assert.Equal(t, &ECDSAWithSHA1, SignatureAlgorithmByX509(x509.ECDSAWithSHA1))
+	assert.Equal(t, &ECDSAWithSHA256, SignatureAlgorithmByX509(x509.ECDSAWithSHA256))
+	assert.Equal(t, &ECDSAWithSHA384, SignatureAlgorithmByX509(x509.ECDSAWithSHA384))
+	assert.Equal(t, &ECDSAWithSHA512, SignatureAlgorithmByX509(x509.ECDSAWithSHA512))
+
+	assert.Panics(t, func() {
+		SignatureAlgorithmByX509(x509.DSAWithSHA1)
+	})
 }
--- xpki/oid/oid_test.go
+++ xpki/oid/oid_test.go
@@ -5,9 +5,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_NewObjectIdentifier(t *testing.T) {
+func Test_NewObjectIdentifierFromOID(t *testing.T) {
 	oids := []asn1.ObjectIdentifier{
 		Data,
 		SignedData,
@@ -34,4 +35,30 @@ func Test_NewObjectIdentifier(t *testing.T) {
 			assert.Equal(t, oidstr, oi.String())
 		})
 	}
+}
+
+func Test_NewObjectIdentifier2(t *testing.T) {
+	t.Run("should fails", func(t *testing.T) {
+		_, err := NewObjectIdentifier("")
+		assert.Error(t, err)
+		var s string
+		_, err = NewObjectIdentifier(s)
+		assert.Error(t, err)
+		_, err = NewObjectIdentifier("1.2.")
+		assert.Error(t, err)
+		_, err = NewObjectIdentifier("1.2.a.3")
+		assert.Error(t, err)
+		_, err = NewObjectIdentifier("{iso(1) member-body(2) us(840) rsadsi(113549) pkcs(1) pkcs-9(9) messageDigest(4}")
+		assert.Error(t, err)
+	})
+
+	t.Run("asn1 notation", func(t *testing.T) {
+		oid1, err := NewObjectIdentifier("{iso(1) member-body(2) us(840) rsadsi(113549) pkcs(1) pkcs-9(9) messageDigest(4)}")
+		require.NoError(t, err)
+
+		oid2, err := NewObjectIdentifier("1.2.840.113549.1.9.4")
+		require.NoError(t, err)
+
+		assert.Equal(t, oid2, oid1)
+	})
 }
