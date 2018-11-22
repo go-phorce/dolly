8de9a838a63b07617a475e520fa996e2d0dc85b5
Denis Issoupov <denis@ekspand.com>
2018-11-21 20:52:35 -0800

    Improved metrics

--- metrics/uptime.go
+++ metrics/uptime.go
@@ -6,15 +6,18 @@ import (
 	"github.com/go-phorce/dolly/metrics/tags"
 )
 
-// PublishHeartbeat publishes heartbeat and uptime of the service
-func PublishHeartbeat(service string, uptime time.Duration) {
-	SetGauge(
-		[]string{"uptime", "seconds", tags.Separator, "service", service},
-		float32(uptime/time.Second),
-	)
-
+// PublishHeartbeat publishes heartbeat of the service
+func PublishHeartbeat(service string) {
 	IncrCounter(
 		[]string{"heartbeat", tags.Separator, "service", service},
 		1,
+	)
+}
+
+// PublishUptime publishes uptime of the service
+func PublishUptime(service string, uptime time.Duration) {
+	SetGauge(
+		[]string{"uptime", "seconds", tags.Separator, "service", service},
+		float32(uptime/time.Second),
 	)
 }
