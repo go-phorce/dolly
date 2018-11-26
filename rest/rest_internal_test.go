c0b82f775b2b409a6876399b8bf1ddf1086776fa
Denis Issoupov <dissoupov@salesforce.com>
2018-11-26 09:10:08 -0800

    notFoundHandler coverage

---
+++ rest/rest_internal_test.go
@@ -0,0 +1,22 @@
+package rest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-phorce/dolly/xhttp/header"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_notFoundHandler(t *testing.T) {
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "/blah", nil)
+	r.RequestURI = "/blah"
+	require.NoError(t, err)
+	notFoundHandler(w, r)
+	assert.Equal(t, http.StatusNotFound, w.Code)
+	assert.Equal(t, header.ApplicationJSON, w.Header().Get(header.ContentType))
+	assert.Equal(t, `{"code":"not_found","message":"/blah"}`, string(w.Body.Bytes()))
+}
