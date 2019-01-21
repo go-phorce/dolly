a4206a1c16e14da318798a4953d9e560847f7df7
Denis Issoupov <dissoupov@salesforce.com>
2019-01-21 09:26:22 -0800

    File reloader on modified time

---
+++ fileutil/reloader/reloader.go
@@ -0,0 +1,134 @@
+package reloader
+
+import (
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/go-phorce/dolly/xlog"
+	"github.com/juju/errors"
+)
+
+var logger = xlog.NewPackageLogger("github.com/go-phorce/dolly/fileutil", "reloader")
+
+// Wrap time.Tick so we can override it in tests.
+var makeTicker = func(interval time.Duration) (func(), <-chan time.Time) {
+	t := time.NewTicker(interval)
+	return t.Stop, t.C
+}
+
+// OnChangedFunc is a called when the file has been modified
+type OnChangedFunc func(filePath string, modifiedAt time.Time)
+
+// Reloader keeps necessary info to provide reloaded certificate
+type Reloader struct {
+	lock           sync.RWMutex
+	loadedAt       time.Time
+	count          uint32
+	filePath       string
+	fileModifiedAt time.Time
+	onChangedFunc  OnChangedFunc
+	inProgress     bool
+	stopChan       chan<- struct{}
+	closed         bool
+}
+
+// NewReloader return an instance of the file re-loader
+func NewReloader(filePath string, checkInterval time.Duration, onChangedFunc OnChangedFunc) (*Reloader, error) {
+	result := &Reloader{
+		filePath:      filePath,
+		onChangedFunc: onChangedFunc,
+		stopChan:      make(chan struct{}),
+	}
+
+	logger.Infof("api=NewReloader, status=started, file=%q", filePath)
+
+	stopChan := make(chan struct{})
+	result.stopChan = stopChan
+	tickerStop, tickChan := makeTicker(checkInterval)
+	go func() {
+		for {
+			select {
+			case <-stopChan:
+				tickerStop()
+				logger.Infof("api=NewReloader, status=closed, count=%d, file=%q",
+					result.LoadedCount(), filePath)
+				return
+			case <-tickChan:
+				modified := false
+				fi, err := os.Stat(filePath)
+				if err == nil {
+					modified = fi.ModTime().After(result.fileModifiedAt)
+					if modified {
+						result.fileModifiedAt = fi.ModTime()
+						err := result.Reload()
+						if err != nil {
+							logger.Errorf("api=NewReloader, err=[%v]", errors.ErrorStack(err))
+						}
+					}
+				} else {
+					logger.Warningf("api=NewReloader, reason=stat, file=%q, err=[%v]", filePath, err)
+				}
+			}
+		}
+	}()
+	return result, nil
+}
+
+// Reload will explicitly call the callback function
+func (k *Reloader) Reload() error {
+	k.lock.Lock()
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Errorf("api=Reload, reason=panic, file=%q, err=[%v]", k.filePath, r)
+		}
+		k.inProgress = false
+		k.lock.Unlock()
+	}()
+
+	if k.inProgress {
+		return nil
+	}
+
+	k.inProgress = true
+
+	atomic.AddUint32(&k.count, 1)
+	k.loadedAt = time.Now().UTC()
+
+	k.onChangedFunc(k.filePath, k.fileModifiedAt)
+
+	return nil
+}
+
+// LoadedAt return the last time when the pair was loaded
+func (k *Reloader) LoadedAt() time.Time {
+	k.lock.RLock()
+	defer k.lock.RUnlock()
+
+	return k.loadedAt
+}
+
+// LoadedCount returns the number of times the pair was loaded from disk
+func (k *Reloader) LoadedCount() uint32 {
+	return atomic.LoadUint32(&k.count)
+}
+
+// Close will close the reloader and release its resources
+func (k *Reloader) Close() error {
+	if k == nil {
+		return nil
+	}
+
+	k.lock.RLock()
+	defer k.lock.RUnlock()
+
+	if k.closed {
+		return errors.New("already closed")
+	}
+
+	k.closed = true
+	k.stopChan <- struct{}{}
+
+	return nil
+}
