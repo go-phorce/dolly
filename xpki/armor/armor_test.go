8c4083a1065d8e07c9ea7f40d8fc4b3758f78920
Denis Issoupov <dissoupov@salesforce.com>
2019-10-17 11:50:06 -0700

    Improving test coverage in armor

--- xpki/armor/armor_test.go
+++ xpki/armor/armor_test.go
@@ -45,3 +45,58 @@ func Test_ArmorDecode(t *testing.T) {
 		})
 	}
 }
+
+func Test_ArmorDecode_Corrupted(t *testing.T) {
+	cases := []struct {
+		file  string
+		count int
+	}{
+		{
+			file:  "testdata/test-gpg-keys-2 corrupted1",
+			count: 1,
+		},
+		{
+			file:  "testdata/test-gpg-keys-2 corrupted2",
+			count: 1,
+		},
+		{
+			file:  "testdata/test-gpg-keys-2 corrupted3",
+			count: 0,
+		},
+		{
+			file:  "testdata/test-gpg-keys-2 corrupted4",
+			count: 0,
+		},
+		{
+			file:  "testdata/test-gpg-keys-2 corrupted5",
+			count: 0,
+		},
+		{
+			file:  "testdata/test-gpg-keys-2 corrupted6",
+			count: 0,
+		},
+	}
+
+	for _, cs := range cases {
+		t.Run(cs.file, func(t *testing.T) {
+			data, err := ioutil.ReadFile(cs.file)
+			require.NoError(t, err)
+
+			count := 0
+			for {
+				block, rest := armor.Decode(data)
+				if block == nil {
+					break
+				}
+				count++
+
+				if rest == nil || len(rest) == 0 {
+					break
+				}
+				data = rest
+			}
+
+			assert.Equal(t, cs.count, count)
+		})
+	}
+}
